a392dafc081904fb1a65364e4ff3efb3db9ea3e8
piyushkp <er.piyushpatel@gmail.com>
2022-01-09 09:33:47 -0800

    more pain

--- yfinance/maxpain.py
+++ yfinance/maxpain.py
@@ -3,38 +3,51 @@ import yfinance as yf
 import options as op
 import pandas as pd
 
+
 def max_pain(date="2022-01-07", symbol="MSFT"):
-    data = op.options_chain_by_date(symbol, date)
-    maxpain = pd.DataFrame()  
-    for value in list(get_strike_prices(data)): 
-        price = value
-        putcash = callcash = 0.0
+    try:
+        data = op.options_chain_by_date(symbol, date)
+        maxpain = pd.DataFrame()  
+        for value in list(get_strike_prices(data)): 
+            price = value
+            putcash = callcash = 0.0
+            for index, row in data.iterrows():
+                strike = row['strike']
+                if strike > price and row["CALL"] == False:
+                    oi = row['openInterest']
+                    if pd.isna(oi) is False:
+                        putcash += (strike - price) * oi * 100
+                if strike < price and row["CALL"] == True:
+                    oi = row['openInterest']
+                    if pd.isna(oi) is False:
+                        callcash += (price - strike) * oi * 100
+            maxpain = maxpain.append({"price" : price, "putcash" : putcash, "callcash": callcash, "total" : putcash + callcash}, ignore_index = True)
+
+        mp = maxpain.sort_values(by="total").iloc[0]["price"]
+        cp = get_current_price(symbol)
+
         for index, row in data.iterrows():
             strike = row['strike']
-            if strike > price and row["CALL"] == False:
-                oi = row['openInterest']
-                if pd.isna(oi) is False:
-                    putcash += (strike - price) * oi * 100
-            if strike < price and row["CALL"] == True:
-                oi = row['openInterest']
-                if pd.isna(oi) is False:
-                    callcash += (price - strike) * oi * 100
-        maxpain = maxpain.append({"price" : price, "putcash" : putcash, "callcash": callcash, "total" : putcash + callcash}, ignore_index = True)
+            if strike == mp:
+                if row["CALL"] == False:
+                    mp_POI = row['openInterest']
+                else:
+                    mp_COI = row['openInterest']
 
-    mp = maxpain.sort_values(by="total").iloc[0]["price"]
-    cp = get_current_price(symbol)
-    #print(maxpain.sort_values(by="total"))
-    print("{0}={1}   {2}   MaxPain: {3}".format(symbol, cp, date, mp))
-    return mp
+        #print(maxpain.sort_values(by="total"))
+        #print("{0}={1}   {2}   MaxPain: {3}".format(symbol, cp, date, mp))
+        return mp, mp_COI, mp_POI
+    except:
+        return None, None, None
 
 def get_index_maxpain():
-    for symbol in ["^spx", "SPY", "^ndx", "QQQ", "^RUT"]:
+    for symbol in ["^ndx", "^spx", "QQQ", "^RUT"]:
         tk = yf.Ticker(symbol)
         exps = tk.options
         cp = get_current_price(symbol)
-        for i in range (0, 20):
-            mp = max_pain(date=exps[i], symbol=symbol)
-            print("{0}={1}   {2}   MaxPain: {3}".format(symbol, cp, exps[i], mp))
+        for index, tuple in enumerate(exps):
+            mp, mp_COI, mp_POI = max_pain(date=exps[index], symbol=symbol)
+            print("{0}={1}   {2}   MaxPain: {3} call_OI: {4}  put_OI: {5}".format(symbol, cp, exps[index], mp, mp_COI, mp_POI))
 
 def get_strike_prices(data):
     strikes = set()
@@ -48,8 +61,5 @@ def get_current_price(symbol):
     return price 
 
 if __name__ == "__main__":
-    print(max_pain(date="2022-01-21", symbol="UBER"))
+    #print(max_pain(date="2022-01-21", symbol="UBER"))
     get_index_maxpain()
-
-
-
---
+++ yfinance/stocks_near_pain.py
@@ -0,0 +1,92 @@
+
+from numpy import NaN, nan
+import yfinance as yf
+import options as op
+import pandas as pd
+from pandas_datareader import data
+from yahoo_fin import stock_info as si
+from maxpain import max_pain, get_current_price
+import logging
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+def get_stocks_around_max_pain(date, symbols):
+    tickers = []
+    count = 1
+    for symbol in symbols:
+        mp, mp_COI, mp_POI = max_pain(date, symbol)
+        if mp is not None:
+            if mp_COI + mp_POI > 10000:
+                tickers.append(symbol)
+                cp = get_current_price(symbol)
+                per = percentage(cp, mp)
+                if (per >= 1 and per <= 20) or (per >= 100 and per <= 120):
+                    print("{0}={1}   {2}   MaxPain: {3}  call_OI: {4}  put_OI: {5}".format(symbol, cp, date, mp, mp_COI, mp_POI))
+        count += 1
+        print(count)
+    print(tickers)
+    return symbol, cp, date, mp, mp_COI, mp_POI
+
+
+def get_big_strangles(date, symbols):
+    for symbol in symbols:
+        mp, mp_COI, mp_POI = max_pain(date, symbol)
+        if mp is not None:
+            if mp_COI + mp_POI > 10000:
+                oi = percentage(mp_COI, mp_POI)
+                if (oi >= 60 and oi <= 100) or (oi>= 100 and oi <= 160):
+                    cp = get_current_price(symbol)
+                    per = percentage(cp, mp)
+                    if (per >= 1 and per <= 20) or (per >= 100 and per <= 120):
+                        print("{0}={1}   {2}   MaxPain: {3}  call_OI: {4}  put_OI: {5}".format(symbol, cp, date, mp, mp_COI, mp_POI))
+    return symbol, cp, date, mp, mp_COI, mp_POI
+
+def get_stock_symbols():
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    # gather stock symbols from major US exchanges
+    #df1 = pd.DataFrame( si.tickers_sp500() )
+    df2 = pd.DataFrame( si.tickers_nasdaq() )
+    df3 = pd.DataFrame( si.tickers_dow() )
+    df4 = pd.DataFrame( si.tickers_other() )
+
+    # convert DataFrame to list, then to sets
+    #sym1 = set( symbol for symbol in df1[0].values.tolist() )
+    sym2 = set( symbol for symbol in df2[0].values.tolist() )
+    sym3 = set( symbol for symbol in df3[0].values.tolist() )
+    sym4 = set( symbol for symbol in df4[0].values.tolist() )
+
+    # join the 4 sets into one. Because it's a set, there will be no duplicate symbols
+    symbols = set.union( sym2, sym3, sym4 )
+    #symbols = set.union( sym1)
+
+
+    # Some stocks are 5 characters. Those stocks with the suffixes listed below are not of interest.
+    my_list = ['W', 'R', 'P', 'Q']
+    del_set = set()
+    sav_set = set()
+    for symbol in symbols:
+        if symbol != "":
+            if len( symbol ) > 4 and symbol[-1] in my_list:
+                del_set.add( symbol )
+            else:
+                try:
+                    market_cap=int(data.get_quote_yahoo(symbol)['marketCap'])
+                    price=int(data.get_quote_yahoo(symbol)['price'])
+                    avdv=int(data.get_quote_yahoo(symbol)['averageDailyVolume3Month'])
+                    # check if market cap is > 5b
+                    if market_cap / 1000000 > 5000 and price > 20 and avdv > 500000:
+                        sav_set.add(symbol)
+                except:
+                    continue
+    print(sav_set)
+    return sav_set
+
+def percentage(part, whole):
+    if whole < 1 :
+        return 0
+    percentage = 100 * float(part)/float(whole)
+    return percentage
+
+if __name__ == "__main__":
+    symbols = get_stock_symbols()
+    get_stocks_around_max_pain(date="2022-01-21", symbols=symbols)
+    get_big_strangles(date="2022-01-21", symbols=symbols)
